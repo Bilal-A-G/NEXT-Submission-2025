2a2585cc515f5908ce9a8007f653fd831c72ebcb
Bilal <86686062+Bilal-A-G@users.noreply.github.com>
2025-01-03 22:29:35 -0500

    Finished trees, they look ok, but are very fast

---
+++ Assets/Scripts/Graphics/BoundsSetter.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class BoundsSetter : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer renderer;
+    [SerializeField] private Vector3 bounds;
+    [SerializeField] private Vector3 center;
+    
+    void Start()
+    {
+        renderer.bounds = new Bounds(renderer.transform.position + center, bounds);
+    }
+
+    private void OnDrawGizmos()
+    {
+        renderer.bounds = new Bounds(renderer.transform.position + center, bounds);
+        Gizmos.DrawWireCube(renderer.bounds.center, renderer.bounds.extents);
+    }
+}
